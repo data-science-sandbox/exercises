99ed7fc1acc590121013d4b203133a3404c5aed1
C <cherrytree@device.local>
2022-02-20 15:48:12 -0500

    first working edit

--- word_cloud/word_cloud.py
+++ word_cloud/word_cloud.py
@@ -1,4 +1,10 @@
-# source: edX UCSanDiego
+"""
+Source: edX, Python for Data Science, UCSanDiegoX
+
+Goal: Instantiate a dictionary, and for every word in the file, add to 
+the dictionary if it doesn't exist. If it does, increase the count.
+
+"""
 
 # Be sure you have followed the instructions to download the 98-0.txt,
 # the text of A Tale of Two Cities, by Charles Dickens
@@ -8,40 +14,32 @@ import collections
 file=open('98-0.txt', encoding="utf8")
 
 # if you want to use stopwords, here's an example of how to do this
-# stopwords = set(line.strip() for line in open('stopwords'))
+stopwords = set(line.strip() for line in open('stopwords'))
 
-# create your data structure here.
+# initialize dictionary data structure
 wordcount={}
-
-# Instantiate a dictionary, and for every word in the file, add to 
-# the dictionary if it doesn't exist. If it does, increase the count.
 
 # Hint: To eliminate duplicates, remember to split by punctuation, 
 # and use case demiliters. The functions lower() and split() will be useful!
 
-# words = re.findall(r'\w+', file.read().lower())
+# iterate through each line
+for line in file:
+    for word in line.split():
+        word = word.lower()
+        word = word.replace(".","")
+        word = word.replace(",","")
+        word = word.replace("\"","")
+        word = word.replace("“","")
+        
+        if word not in stopwords:
+            if word not in wordcount:
+                wordcount[word] = 1
+            else:
+                wordcount[word] += 1
 
-# counter = collections.Counter()
-# for word in file.read().lower():
-#     counter[word] += 1
-# counter
+counter = collections.Counter(wordcount)
 
-for word in #code here
-    word = word.replace(".","")
-    word = word.replace(",","")
-    word = word.replace("\"","")
-    word = word.replace("“","")
-    # if word not in stopwords:
-    #     if word not in wordcount:
-    #         wordcount[word] = 1
-    #     else:
-    #         wordcount[word] += 1
+top_ten = counter.most_common(10)
 
-# after building your wordcount, you can then sort it and return the first
-# n words.  If you want, collections.Counter may be useful.
-
-# d = collections.Counter(wordcount)
-
-#print(d.most_common(10))
-# for word, count in d.most_common(10):
-# 	print(word, ": ", count)
+for word, count in counter.most_common(10):
+ 	print(word, ": ", count)
