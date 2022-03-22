fad94d410bb666e4bddb5ce2fe42385fd38c5bc7
C <99933422+data-science-sandbox@users.noreply.github.com>
2022-03-21 22:15:44 -0400

    Initial commit. Visualization practice sandbox

---
+++ CO2_emissions_visualizations.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Initial commit: 2022 03
+
+Adapted from "Python for Data Science" by UCSanDiegoX, an edX class
+"""
+
+import pandas as pd
+import numpy as np
+import seaborn as sn
+import matplotlib
+import matplotlib.pyplot as plt
+
+# World Development Indicators dataset from Kaggle
+data = pd.read_csv('Indicators.csv')
+
+# select CO2 emissions for the United States
+indicator = 'CO2 emissions \(metric'
+country = 'USA'
+
+# boolean masks
+mask1 = data['IndicatorName'].str.contains(indicator) 
+mask2 = data['CountryCode'].str.contains(country)
+
+# df containing data of interest
+subset = data[mask1 & mask2]
+
+# font
+matplotlib.rcParams['font.family'] = 'Roboto'
+
+# line plot
+plt.plot(subset['Year'].values, subset['Value'].values)
+
+# labels
+plt.xlabel('Year')
+plt.ylabel(subset['IndicatorName'].iloc[0])
+plt.title('')
+
+# axis limits
+plt.axis([1959, 2015, 0, 25])
+
+plt.show()
