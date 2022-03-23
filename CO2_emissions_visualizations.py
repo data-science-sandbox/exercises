f024da5d90293213f9e281d4a7e214d2a3603a5d
C <99933422+data-science-sandbox@users.noreply.github.com>
2022-03-22 22:17:46 -0400

    Add seaborn line plot version

--- CO2_emissions_visualizations.py
+++ CO2_emissions_visualizations.py
@@ -8,7 +8,7 @@ Adapted from "Python for Data Science" by UCSanDiegoX, an edX class
 
 import pandas as pd
 import numpy as np
-import seaborn as sn
+import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -27,17 +27,22 @@ mask2 = data['CountryCode'].str.contains(country)
 subset = data[mask1 & mask2]
 
 # font
-matplotlib.rcParams['font.family'] = 'Roboto'
+matplotlib.rcParams['font.family'] = 'Helvetica'
 
-# line plot
+# matplotlib version
+plt.figure()
 plt.plot(subset['Year'].values, subset['Value'].values)
-
 # labels
 plt.xlabel('Year')
 plt.ylabel(subset['IndicatorName'].iloc[0])
 plt.title('')
-
 # axis limits
 plt.axis([1959, 2015, 0, 25])
-
 plt.show()
+
+# seaborn version
+plt.figure()
+sns.lineplot(data=subset, x='Year', y='Value')
+plt.xlabel('Year')
+plt.ylabel(subset['IndicatorName'].iloc[0])
+plt.axis([1959, 2015, 0, 25])
